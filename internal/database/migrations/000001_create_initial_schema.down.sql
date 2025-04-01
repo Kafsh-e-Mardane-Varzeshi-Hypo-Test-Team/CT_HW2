17c39e1577c2a9a3138e9dfa2cca5b1ce1363229
Parisa Mousavi <mousaviparisa5@yahoo.com>
2025-04-01 13:53:08 +0330

    feat: update 000001 down migration

--- internal/database/migrations/000001_create_initial_schema.down.sql
+++ internal/database/migrations/000001_create_initial_schema.down.sql
@@ -1,14 +1,16 @@
+-- Drop triggers first
+DROP TRIGGER IF EXISTS after_submission_insert ON submissions;
+DROP TRIGGER IF EXISTS after_submission_update ON submissions;
+
+-- Drop function
+DROP FUNCTION IF EXISTS update_submission_stats();
+
 -- Drop tables
 DROP TABLE IF EXISTS submissions;
 DROP TABLE IF EXISTS test_cases;
 DROP TABLE IF EXISTS problems;
+DROP TABLE IF EXISTS user_stats;
 DROP TABLE IF EXISTS users;
-
--- Drop triggers
-DROP TRIGGER IF EXISTS update_problems_modtime ON problems;
-
--- Drop functions
-DROP FUNCTION IF EXISTS update_modified_column();
 
 -- Drop enum types
 DROP TYPE IF EXISTS submission_status;
