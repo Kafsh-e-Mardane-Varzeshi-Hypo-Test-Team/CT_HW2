3c03723323550eddad047dbb2b7638cb5140c145
mammedbrk <mammedbrk@gmail.com>
2025-04-28 17:51:46 +0330

    feat: create api request and response model

---
+++ internal/api/model.go
@@ -0,0 +1,15 @@
+package api
+
+type ExecuteRequest struct {
+	Code           string `json:"code" binding:"required"`
+	Input          string `json:"input" binding:"required"`
+	ExpectedOutput string `json:"expected_output" binding:"required"`
+	MemoryLimit    int    `json:"memory_limit" binding:"required"`
+	Timeout        int    `json:"timeout" binding:"required"`
+}
+
+type ExecuteResponse struct {
+	Status     string `json:"status"`
+	TimeUsed   int    `json:"time_used"`
+	MemoryUsed int    `json:"memory_used"`
+}
